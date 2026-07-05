9b47f07149483f96a300d33999bb5f131c72daee
Darshanwalher <darshanwalher21@gmail.com>
2026-07-05 18:12:43 +0530

    feat: add cat recommendation and retrieval tools
    - Implemented `recommendCatsTool` and `getAllCatsTool` for cat recommendations and fetching all cats data.
    - Registered tools in the MCP server with appropriate input schemas and async handlers.
    - Updated server initialization in `index.ts` to include new tools and connect via `StdioServerTransport`.
    - Added `ts-node` and `tsx` as dependencies for TypeScript execution and development.
    - Updated package scripts for development and building.

--- mcp_server/src/index.ts
+++ mcp_server/src/index.ts
@@ -1,6 +1,7 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
+import { getAllCatsTool, recommendCatsTool } from "./tools/recommendCats.tool.ts";
 
 
 // Create server instance
@@ -8,3 +9,51 @@ const server = new McpServer({
   name: "tiny_cats",
   version: "1.0.0",
 });
+
+server.registerTool(
+    "recommend_cats",
+    {
+        title: "Recommend Cats",
+        description: "Recommend best cat breeds according to the user's preferences",
+        inputSchema: {
+            kidsFriendly: z.boolean().describe("Whether the cat should be friendly with kids or not"),
+            apartmentFriendly: z.boolean().describe("Whether the user lives in an apartment or not"),
+        }
+    },
+    async ({kidsFriendly, apartmentFriendly}) => {
+        const result = await recommendCatsTool(kidsFriendly, apartmentFriendly);
+        return {
+            content: [
+                {
+                    type: "text",
+                    text: JSON.stringify(result)
+                }
+            ]
+        }
+    }
+)
+
+server.registerTool(
+  "get_all_cats",
+  {
+    title: "all cats",
+    description: "cats Data",
+  },
+  async () => {
+    const result = await getAllCatsTool();
+
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(result),
+        },
+      ],
+    };
+  }
+);
+
+const transport = new StdioServerTransport();
+await server.connect(transport);
+
+console.log("tiny cats mcp server is running...");
